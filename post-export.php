221669c8e0f2fcb58b2805e5ccc7a07a98adce4b
Axel Ilali <axel.ilali@yahoo.fr>
2023-06-15 00:19:18 +0100

    initial commit

---
+++ post-export.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Post Export
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Simple plugin to export any post type in a CSV file
+ * Author:          Axel Ilali
+ * Author URI:      https://axel-ilali.cm
+ * Text Domain:     post-export
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Post_Export
+ */
+
+// Your code starts here.
